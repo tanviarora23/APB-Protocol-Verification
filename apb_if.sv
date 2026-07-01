67b8eae285b16efa06f37b4d8f0a64963dd74a67
tanviarora23 <66714549+tanviarora23@users.noreply.github.com>
2026-07-01 20:17:08 +0530

    Create apb_if.sv

---
+++ apb_if.sv
@@ -0,0 +1,18 @@
+interface apb_if(input logic PCLK);
+
+    logic PRESETn;
+
+    logic PSEL;
+    logic PENABLE;
+    logic PWRITE;
+
+    logic [7:0]  PADDR;
+    logic [31:0] PWDATA;
+    logic [31:0] PRDATA;
+
+    logic PREADY;
+    logic PSLVERR;
+
+endinterface
+
+
