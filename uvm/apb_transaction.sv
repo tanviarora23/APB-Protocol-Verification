8ca5275ec075ceb30573f8a28ba75163ec3bf592
tanviarora23 <66714549+tanviarora23@users.noreply.github.com>
2026-07-02 12:47:28 +0530

    Update apb_transaction.sv

--- uvm/apb_transaction.sv
+++ uvm/apb_transaction.sv
@@ -1,3 +1,45 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class apb_transaction extends uvm_sequence_item;
+
+  `uvm_object_utils(apb_transaction)
+
+  // Randomizable fields
+  rand bit        PWRITE;
+  rand bit [7:0]  PADDR;
+  rand bit [31:0] PWDATA;
+
+  // Response field
+  bit [31:0] PRDATA;
+
+  // Constructor
+  function new(string name = "apb_transaction");
+    super.new(name);
+  endfunction
+
+endclass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''''
 class apb_transaction extends uvm_sequence_item;
 
     rand bit PWRITE;
@@ -9,3 +51,4 @@ class apb_transaction extends uvm_sequence_item;
     bit [31:0] PRDATA;
 
 endclass
+''''
