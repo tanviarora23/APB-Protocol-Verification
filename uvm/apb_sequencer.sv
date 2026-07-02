7dbb59df4c24dbcbb4735e9883a929b88ff556c5
tanviarora23 <66714549+tanviarora23@users.noreply.github.com>
2026-07-02 17:09:21 +0530

    Added APB sequencer for transaction management

---
+++ uvm/apb_sequencer.sv
@@ -0,0 +1,13 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class apb_sequencer extends uvm_sequencer #(apb_transaction);
+
+  `uvm_component_utils(apb_sequencer)
+
+  function new(string name = "apb_sequencer",
+               uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+endclass
