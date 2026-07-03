a794562a465bc70510cdd8aa221c345a27512b7d
tanviarora23 <66714549+tanviarora23@users.noreply.github.com>
2026-07-03 12:33:50 +0530

    Update top.sv

--- uvm/top.sv
+++ uvm/top.sv
@@ -7,12 +7,32 @@ module top;
 
   apb_if apbif(PCLK);
 
+  apb_slave dut(
+      .PCLK(PCLK),
+      .PRESETn(apbif.PRESETn),
+      .PSEL(apbif.PSEL),
+      .PENABLE(apbif.PENABLE),
+      .PWRITE(apbif.PWRITE),
+      .PADDR(apbif.PADDR),
+      .PWDATA(apbif.PWDATA),
+      .PRDATA(apbif.PRDATA),
+      .PREADY(apbif.PREADY),
+      .PSLVERR(apbif.PSLVERR)
+  );
+
   initial begin
     PCLK = 0;
     forever #5 PCLK = ~PCLK;
   end
 
   initial begin
+    apbif.PRESETn = 0;
+    #20;
+    apbif.PRESETn = 1;
+  end
+
+  initial begin
+    uvm_config_db#(virtual apb_if)::set(null, "*", "vif", apbif);
     run_test("apb_test");
   end
 
